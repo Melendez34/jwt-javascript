6e9c5a3e487c1f2c92e8b23e229298865ec065b3
Omar Melendez <omar.melendez@tvazteca.com.mx>
2022-06-22 17:03:51 -0500

    Implementando autenticacion en server.js y agregando dependencia de bcrypt

--- server.js
+++ server.js
@@ -1,26 +1,61 @@
+const bcrypt = require('bcrypt');
 const dotenv = require('dotenv');
 const express = require('express');
 const app = express();
 const jwt = require('jsonwebtoken');
 dotenv.config();
+app.use(express.json());
+
 const posts = [
-  {
+  /*{
     username: 'Omar',
     title: 'post 1',
   },
   {
     username: 'Guadalupe',
     title: 'post 2',
-  },
+  },*/
   {
     username: 'Luis',
     title: 'post 3',
-  },
+  }
 ];
 
-app.use(express.json());
+app.get('/users', (req,res) => {
+  res.json(posts)
+})
 
-app.get('/posts', authenticationToken, (req, res) => {
+app.post('/users', async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt()
+    const hashedPass = await bcrypt.hash(req.body.password, 10)
+    console.log(salt);
+    console.log(hashedPass);
+    const user = { username:req.body.name, password: hashedPass }
+    posts.push(user)
+    res.sendStatus(201)
+  } catch {
+    res.sendStatus(500)
+  }
+})
+
+app.post('/users/login', async (req, response) => {
+  const user = posts.find(user => user.username == req.body.name)
+  if(user == null){
+    return response.sendStatus(400)
+  }
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      response.send("Success")
+    } else {
+      response.send("Not allowed")
+    }
+  } catch {
+    response.sendStatus(500)
+  }
+})
+
+app.get('/posts', authorizationToken, (req, res) => {
   res.json(posts.filter((post) => post.username === req.user.name));
 });
 
@@ -32,7 +67,7 @@ app.post('/login', (req, res) => {
   res.json({ accessToken: accessToken });
 });
 
-function authenticationToken(request, response, next) {
+function authorizationToken(request, response, next) {
   const authHeader = request.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
   if (token == null) {
